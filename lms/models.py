d8c0ae4c27b2b5dadc586b048f09295f4d7c2be4
Arkady V. Savelev <saversavic@gmail.com>
2025-01-20 16:30:51 +0500

    add sample.env, add models lesson and course

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название'
+    )
+
+    preview = models.ImageField(
+        upload_to='media/lms',
+        blank=True,
+        null=True,
+        verbose_name='Изображение')
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['name']
+
+class Lesson(models.Model):
+
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название'
+    )
+
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+
+    preview = models.ImageField(
+        upload_to='media/learning',
+        blank=True,
+        null=True,
+        verbose_name='Изображение'
+    )
+
+    videolink = models.URLField(
+        max_length=250,
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на видео'
+    )
+
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='lessons',
+        verbose_name='Курс'
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ['name']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Адрес электрической почты',
+        help_text='Укажите адрес электрической почты')
+
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name='Телефон',
+        help_text='Укажите номер телефона')
+
+    city = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Город',
+        help_text='Укажите название города')
+
+    avatar = models.ImageField(
+        upload_to='users/avatars',
+        blank=True,
+        null=True,
+        verbose_name='Аватар',
+        help_text='Загрузите аватара')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'пользователи'
